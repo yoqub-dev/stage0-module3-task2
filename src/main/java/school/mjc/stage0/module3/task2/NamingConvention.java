580b8371fb3b41009c8af76cd2d5435182057b8d
yoqub-dev <yoqub.epam@gmail.com>
2023-10-29 16:43:07 +0500

    task2

--- src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
@@ -3,13 +3,13 @@ package school.mjc.stage0.module3.task2;
 public class NamingConvention {
     public static void main(String[] args) {
         int _variable = 10;
-        int variable_One = 1;
+        int variable_one = 1;
         int variableTwo = 2;
         int variable_three = 3;
-        final int VARIABLEFOUR = 4;
-        final int VARIABLEfIVE = 5;
-        int VariableSix = 6;
-        int variableseven = 7;
-        int VariableEight = 8;
+        final int variableFour = 4;
+        final int variableFive = 5;
+        int variableSix = 6;
+        int variableSeven = 7;
+        int variableEight = 8;
     }
 }
